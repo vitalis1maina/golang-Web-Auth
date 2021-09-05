6ec8baa9f6cb3716e6be9ab326ba8ed26fb600eb
vitalis <vitalismaina16@gmail.com>
2021-09-05 19:34:44 +0300

    encoding example

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -13,32 +12,38 @@ type person struct {
 
 func main() {
 
-// person1 := person{
-// 	First: "vitalis",
-// }
-// person2 := person{
-// 	First: "maina",
-// }
-// slicePerson := []person{person1, person2}
-// bs, err := json.Marshal(slicePerson)
-// if err != nil {
-// 	log.Panic(err) //panic when u get a programmer error
-// }
-// fmt.Println("PRINT JSON:", string(bs))
-// slicePerson2 := []person{}
-// err = json.Unmarshal(bs, &slicePerson2)
-// if err != nil {
-// 	log.Panic(err)
-// }
-// fmt.Println("Unmashalled data:", slicePerson2)
+	// person1 := person{
+	// 	First: "vitalis",
+	// }
+	// person2 := person{
+	// 	First: "maina",
+	// }
+	// slicePerson := []person{person1, person2}
+	// bs, err := json.Marshal(slicePerson)
+	// if err != nil {
+	// 	log.Panic(err) //panic when u get a programmer error
+	// }
+	// fmt.Println("PRINT JSON:", string(bs))
+	// slicePerson2 := []person{}
+	// err = json.Unmarshal(bs, &slicePerson2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("Unmashalled data:", slicePerson2)
 
-	htttp.Handlefunc("/encode",encode)
-	http.HandleFunc("/decode",decode)
-	http.ListenAndServe(":8080",nil)
+	http.HandleFunc("/encode", encode)
+	http.HandleFunc("/decode", decode)
+	http.ListenAndServe(":8080", nil)
 }
-func encode(http.ResponseWriter r *http.Request){
+func encode(w http.ResponseWriter, r *http.Request) {
+	p1 := person{
+		First: "vee",
+	}
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println("Encode bad data", err)
+	}
+}
+func decode(w http.ResponseWriter, r *http.Request) {
 
-}
-func decode(http.ResponseWriter r * http.Request){
-	
 }
