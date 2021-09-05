8a90b85b5f6627389adb684b4c4c075213f6d6ec
vitalis <vitalismaina16@gmail.com>
2021-09-05 20:35:20 +0300

    exercise1

--- main.go
+++ main.go
@@ -7,49 +7,36 @@ import (
 )
 
 type person struct {
-	First string
+	Name string
 }
 
 func main() {
 
-	// person1 := person{
-	// 	First: "vitalis",
-	// }
-	// person2 := person{
-	// 	First: "maina",
-	// }
-	// slicePerson := []person{person1, person2}
-	// bs, err := json.Marshal(slicePerson)
-	// if err != nil {
-	// 	log.Panic(err) //panic when u get a programmer error
-	// }
-	// fmt.Println("PRINT JSON:", string(bs))
-	// slicePerson2 := []person{}
-	// err = json.Unmarshal(bs, &slicePerson2)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	// fmt.Println("Unmashalled data:", slicePerson2)
-
 	http.HandleFunc("/encode", encode)
 	http.HandleFunc("/decode", decode)
 	http.ListenAndServe(":8080", nil)
+
 }
 func encode(w http.ResponseWriter, r *http.Request) {
 	p1 := person{
-		First: "vee",
+		"vitalis",
 	}
-	err := json.NewEncoder(w).Encode(p1)
+	p2 := person{
+		"maina",
+	}
+	people := []person{p1, p2}
+
+	err := json.NewEncoder(w).Encode(people)
 	if err != nil {
-		log.Println("Encode bad data", err)
+		log.Println("bad data to encode", err)
 	}
+
 }
 func decode(w http.ResponseWriter, r *http.Request) {
-	var p1 person
-	err := json.NewDecoder(r.Body).Decode(&p1)
+	people := []person{}
+	err := json.NewDecoder(r.Body).Decode(&people)
 	if err != nil {
-		log.Println(err)
+		log.Println("decoding bad data:", err)
 	}
-	log.Println(p1)
-
+	log.Println(people)
 }
