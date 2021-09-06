9d632f1ef1b14f14e6d2b8b5035285f64d09b0f3
vitalis <vitalismaina16@gmail.com>
2021-09-06 21:36:59 +0300

    hashing example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vitalis1maina/golang-Web-Auth
 
 go 1.17
+
+require golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
+	//use cases
+	password := "vi&%$#@323vead^!9089u"
+	hashedPass, err := hashedPassword(password)
+	err = comparePassword(password, hashedPass)
+	if err != nil {
+		log.Fatalln("Your are not loggedin")
+	}
+	log.Println("logged in!")
+}
 
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+func hashedPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error while generating hashed password from bycrpt:%w", err)
+	}
+	return bs, nil
+}
+func comparePassword(password string, hashedPass []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
+	if err != nil {
+		return fmt.Errorf("Invalid/Wrong Password:%w", err)
+	}
+	return nil
 }
