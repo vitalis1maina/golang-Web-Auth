6c6f30509c02d3fedac66d1adeac8114a7c387d9
vitalis <vitalismaina18@gmail.com>
2021-09-17 11:42:12 +0300

    tribonacci sequence

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/vitalis1maina/golang-Web-Auth
 
 go 1.17
-
-require golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
--- main.go
+++ main.go
@@ -1,58 +1,20 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha512"
 	"fmt"
-	"log"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
-var key []byte
-
 func main() {
-	//use cases
-	for i := 1; i <= 64; i++ {
-		key = append(key, byte(i))
+	n := 5
+	for i := 0; i <= n; i++ {
+		fmt.Println(fibonacci(i))
 	}
-	password := "vi&%$#@323vead^!9089u"
-	hashedPass, err := hashedPassword(password)
-	err = comparePassword(password, hashedPass)
-	if err != nil {
-		log.Fatalln("Your are not loggedin")
-	}
-	log.Println("logged in!")
 }
-
-func hashedPassword(password string) ([]byte, error) {
-	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return nil, fmt.Errorf("Error while generating hashed password from bycrpt:%w", err)
+func fibonacci(n int) int {
+	if n == 0 || n == 1 {
+		return 0
+	} else if n == 2 {
+		return 1
 	}
-	return bs, nil
-}
-func comparePassword(password string, hashedPass []byte) error {
-	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
-	if err != nil {
-		return fmt.Errorf("Invalid/Wrong Password:%w", err)
-	}
-	return nil
-}
-func signMessage(msg []byte) ([]byte, error) {
-	h := hmac.New(sha512.New, key)
-	_, err := h.Write(msg)
-	if err != nil {
-		return nil, fmt.Errorf("error in sign message while hashing")
-	}
-	signature := h.Sum(nil)
-	return signature, nil
-}
-func checksig(msg, sig []byte) (bool, error) {
-	newsig, err := signMessage(msg)
-	if err != nil {
-		return false, fmt.Errorf("error in checkSig while getting signature of message: %w", err)
-	}
-	same := hmac.Equal(newsig, sig)
-	return same, nil
+	return fibonacci(n-1) + fibonacci(n-2) + fibonacci(n-3)
 }
